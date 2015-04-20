6285fa3b00728cd4fdcf95379466069391d808ee
maisoon al quran <maisoonalquran@maisoons-MacBook-Pro.local>
2015-04-20 19:33:23 +0300

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by maisoon al quran on 4/20/15.
+//  Copyright (c) 2015 maisoon al quran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
